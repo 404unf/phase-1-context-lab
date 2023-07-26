90c2ea9dca3b2ad5c47f39b37a436faca0f0354c
Anna <34191811+404unf@users.noreply.github.com>
2023-07-25 20:14:39 -0700

    Payroll application utilizing context

--- index.js
+++ index.js
@@ -1,5 +1,93 @@
 /* Your Code Here */
 
+function createEmployeeRecord([firstName, familyName, title, payRate]) {
+    return {
+        firstName: firstName,
+        familyName: familyName,
+        title: title,
+        payPerHour: payRate,
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(arrayOfArrays) {
+    let arrayOfObjects = arrayOfArrays.map(array => createEmployeeRecord(array))
+    return arrayOfObjects
+
+}
+
+function createTimeInEvent(dateStamp) {
+    const dateTime = dateStamp.split(' ')
+    const hour = parseInt(dateTime[1])
+    const date = dateTime[0]
+
+    this.timeInEvents.push({
+        type: 'TimeIn',
+        hour: hour,
+        date: date
+    })
+    return this
+}
+
+function createTimeOutEvent(dateStamp) {
+    const dateTime = dateStamp.split(' ')
+    const hour = parseInt(dateTime[1])
+    const date = dateTime[0]
+
+    this.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: hour,
+        date: date
+    })
+    return this
+}
+
+function hoursWorkedOnDate(date) {
+    const listTimeIn = this.timeInEvents
+    const listTimeOut = this.timeOutEvents
+    const timeIn = []
+    listTimeIn.forEach(timeEvent => { if (timeEvent.date === date) { timeIn.push(timeEvent.hour) } })
+
+    const timeOut = []
+    listTimeOut.forEach(timeEvent => { if (timeEvent.date === date) { timeOut.push(timeEvent.hour) } })
+
+    const hours = timeOut - timeIn
+
+    return hours / 100
+}
+
+function wagesEarnedOnDate(date) {
+    const hours = hoursWorkedOnDate.call(this, date)
+    const payRate = this.payPerHour
+
+    const payOwed = hours * payRate
+
+    return payOwed
+
+}
+
+// (function allWagesFor() {
+//     let datesWorked = []
+//     datesWorked = this.timeInEvents.map(timeEvent => timeEvent.date)
+
+//     let allWages = datesWorked.reduce((accumulator, date) => accumulator + wagesEarnedOnDate(object, date), 0)
+
+//     return allWages
+// }).bind(this)
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    for (let i = 0; i < srcArray.length; i++) {
+        if (firstName === srcArray[i].firstName) { return srcArray[i] }
+    }
+}
+
+function calculatePayroll(arrayOfEmployees) {
+    let sumOfAllPayOwed = arrayOfEmployees.reduce((accumulator, employeeRecord) => accumulator + allWagesFor.call(employeeRecord), 0)
+
+    return sumOfAllPayOwed
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
